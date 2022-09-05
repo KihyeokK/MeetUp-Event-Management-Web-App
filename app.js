92abfd83623fbbfccef33d617d543c1485fc481a
KihyeokK <joe.brighter20@gmail.com>
2022-09-04 20:06:53 -0400

    Fix port number for deployment

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ mongoose
   .connect(MONGODB_URI)
   .then((success) => {
     console.log("connected");
-    app.listen(3000);
+    app.listen(process.env.PORT || 3000);
   })
   .catch((err) => {
     console.log(err);
